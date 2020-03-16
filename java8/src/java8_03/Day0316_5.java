43fb05ec2bf508ca4d651c73c5fbc6d720630928
senghoon <seunghoon.ryu.dev@gmail.com>
2020-03-16 14:11:11 +0900

    3일차 관계연산자

---
+++ java8/src/java8_03/Day0316_5.java
@@ -0,0 +1,32 @@
+package java8_03;
+
+public class Day0316_5 {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 20;
+		int num3 = 20;
+		
+		System.out.println("num1 < num2 : " + (num1 < num2));
+		System.out.println("num1 <= num2 : " + (num1 <= num2));	
+		System.out.println("num1 > num2 : " + (num1 > num2));	
+		System.out.println("num1 >= num2 : " + (num1 >= num2));	
+		System.out.println("num1 == num2 : " + (num1 == num2));	
+		System.out.println("num1 != num2 : " + (num1 != num2));	
+		
+		System.out.println( "\n num2 = 20, num3 = 20" );	
+		System.out.println("num3 < num2 : " + (num3 < num2));	
+		System.out.println("num3 <= num2 : " + (num3 <= num2));	
+		System.out.println("num3 > num2 : " + (num3 > num2));	
+		System.out.println("num3 >= num2 : " + (num3 >= num2));	
+		System.out.println("num3 == num2 : " + (num3 == num2));	
+		System.out.println("num3 != num2 : " + (num3 != num2));
+		
+		boolean result = num1 < num2;
+		System.out.println(result);
+		
+		
+
+	}
+
+}
