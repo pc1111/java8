d46a7d67a17a7138d05956700674f0208fab7b0b
senghoon <seunghoon.ryu.dev@gmail.com>
2020-03-16 14:57:32 +0900

    3일차 대입연산자

---
+++ java8/src/java8_03/Day0316_7.java
@@ -0,0 +1,25 @@
+package java8_03;
+
+public class Day0316_7 {
+
+	public static void main(String[] args) {
+		int a = 10, b = 3;
+		a += b;
+		System.out.println(a);
+		a -= b;
+		System.out.println(a);
+		a *= b;
+		System.out.println(a);
+		
+		a = 10;
+		System.out.println(a);
+		a *= 3 + 10;
+		System.out.println(a);
+		
+		a += 1;
+		System.out.println(a);
+		a += 1;
+		System.out.println(a);
+	}
+
+}
