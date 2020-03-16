ea5b3524746d554778993e7d3d2417d20dc808d3
senghoon <seunghoon.ryu.dev@gmail.com>
2020-03-16 15:17:15 +0900

    3일차 수업이길다ㅠ.ㅠ

---
+++ java8/src/java8_03/Day0316_8.java
@@ -0,0 +1,60 @@
+package java8_03;
+
+public class Day0316_8 {
+	public static void main(String[] args) {
+		int a = 10;
+		System.out.println(a);
+		a+=1;
+		System.out.println(a);
+		a+=1;
+		System.out.println(a);
+		a+=1;
+		System.out.println(a);
+		
+		
+		a++;
+		System.out.println(a);
+		a++;
+		System.out.println(a);
+		a++;
+		System.out.println(a);
+
+		++a;
+		System.out.println(a);
+		++a;
+		System.out.println(a);
+		++a;
+		System.out.println(a);
+		
+		a--;
+		System.out.println(a);
+		a--;
+		System.out.println(a);
+		a--;
+		System.out.println(a);
+
+		--a;
+		System.out.println(a);
+		--a;
+		System.out.println(a);
+		--a;
+		System.out.println(a);
+		System.out.println();
+		
+		a=10;
+		System.out.println(++a);
+		System.out.println(++a);
+		System.out.println(++a);
+		System.out.println(++a);
+		System.out.println(++a);
+		System.out.println();
+		
+		a=10;
+		System.out.println(a++);
+		System.out.println(a++);
+		System.out.println(a++);
+		System.out.println(a++);
+		System.out.println(a++);
+	}
+	
+}
