c3648e418c36350c9b64da333cd05f52e82e37eb
senghoon <seunghoon.ryu.dev@gmail.com>
2020-03-20 16:25:23 +0900

    6일차 continue

---
+++ java8/src/java8_06/Day0320_13.java
@@ -0,0 +1,17 @@
+package java8_06;
+
+public class Day0320_13 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 10; i++)
+		{
+			if(i%2 != 0) {
+				continue;
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}
