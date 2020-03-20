ddf422a8f143ad2057c42f23be90e11acc4597ea
senghoon <seunghoon.ryu.dev@gmail.com>
2020-03-20 13:25:26 +0900

    6일차 for

---
+++ java8/src/java8_06/Day0320_01.java
@@ -0,0 +1,16 @@
+package java8_06;
+
+public class Day0320_01 {
+
+	public static void main(String[] args) {
+		int sum =0;
+		
+		for(int i = 1 ; i <= 10  ; i++)
+		{
+			sum += i;
+		}
+		
+		System.out.println("1부터 10까지의 합 : " + sum);
+	}
+
+}
