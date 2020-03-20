0a6151861652e0fc68af607b1fc7c496754876c9
senghoon <seunghoon.ryu.dev@gmail.com>
2020-03-20 13:33:51 +0900

    6일차 for 추가내용

---
+++ java8/src/java8_06/Day0320_02.java
@@ -0,0 +1,18 @@
+package java8_06;
+
+public class Day0320_02 {
+
+	public static void main(String[] args) {
+		int sum =0;
+		
+		for(int i = 1 ; i <= 100  ; i++)
+		{
+			sum += i;
+		}
+		
+		System.out.println("1부터 100까지의 합 : " + sum);
+		//System.out.println("현재 i의 값 : " + i);
+		
+	}
+
+}
