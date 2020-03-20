d7f70f9edd25c8ad1de61e38c1ab1ad2f3996615
senghoon <seunghoon.ryu.dev@gmail.com>
2020-03-20 16:07:35 +0900

    6일차 while 추가설명

---
+++ java8/src/java8_06/Day0320_08.java
@@ -0,0 +1,15 @@
+package java8_06;
+
+public class Day0320_08 {
+
+	public static void main(String[] args) {
+		int i = 0;
+		
+		while(i<5) { //조건에 true를 입력하면 무한반복문이 됩니다.
+			System.out.println("Hello!");
+			i++;
+		}
+
+	}
+
+}
