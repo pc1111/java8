e1e997442edfa1f5518c9198393886c2c76525ee
senghoon <seunghoon.ryu.dev@gmail.com>
2020-03-20 14:01:52 +0900

    6일차 for 추가

---
+++ java8/src/java8_06/Day0320_03.java
@@ -0,0 +1,20 @@
+package java8_06;
+
+public class Day0320_03 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0 ; i < 10 ; i++)
+		{
+			System.out.println(i);
+		}
+		
+		for(int i = 0 ; i < 5 ; i++)
+		{
+			System.out.println("hello!");
+		}
+		
+		
+	}
+
+}
