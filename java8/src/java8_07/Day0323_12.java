56927420f0f2b5ef2f2258c380b4f0a0db533a85
senghoon <seunghoon.ryu.dev@gmail.com>
2020-03-23 13:13:17 +0900

    7일차 특수문자로 삼각형

---
+++ java8/src/java8_07/Day0323_12.java
@@ -0,0 +1,27 @@
+package java8_07;
+
+import java.util.Scanner;
+
+public class Day0323_12 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("몇 줄을 출력하시겠습니까? : ");
+		
+		int line = s.nextInt();
+		
+		for(int i = 0; i < line ; i++)
+		{
+			for(int j = 0; j <= i ; j++)
+			{
+				if(j % 2 == 0)
+					System.out.print("$");
+				else
+					System.out.print("@");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
