4a4673945b099e42abe71be7bf64fffd2132c63a
senghoon <seunghoon.ryu.dev@gmail.com>
2020-03-23 11:36:24 +0900

    7일차 1~100 ?배수에 추가맨트

--- java8/src/java8_07/Day0323_11.java
+++ java8/src/java8_07/Day0323_11.java
@@ -13,6 +13,18 @@ public class Day0323_11 {
 			}
 		}
 
+		for(int i = 1 ; i <= 100 ; i++)
+		{
+			if(i % 15 == 0) System.out.print(i + "PING PONG\t");
+			else if(i % 5 == 0) System.out.print(i + "PONG\t");
+			else if( i % 3 == 0)System.out.print(i + "PING\t");
+			else System.out.print(i + "\t");
+			
+			if(i % 10 == 0)
+				System.out.println();
+			
+		}
+
 
 	}
 
