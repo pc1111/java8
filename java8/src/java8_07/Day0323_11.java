9f4bd8a0471cf9662471e525c9b53eee04bddb75
senghoon <seunghoon.ryu.dev@gmail.com>
2020-03-23 11:12:20 +0900

    7일차 1에서 100까지 출력

---
+++ java8/src/java8_07/Day0323_11.java
@@ -0,0 +1,19 @@
+package java8_07;
+
+public class Day0323_11 {
+
+	public static void main(String[] args) {
+
+		for(int i = 1 ; i <= 100 ; i++)
+		{
+			System.out.print(i + "\t");
+			if(i % 10 == 0)
+			{
+				System.out.println();
+			}
+		}
+
+
+	}
+
+}
