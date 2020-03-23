0f1444b68f961df4784035f18610be36ab1a462a
senghoon <seunghoon.ryu.dev@gmail.com>
2020-03-23 10:06:14 +0900

    7일차 3의 배수의 합

---
+++ java8/src/java8_07/Day0323_05.java
@@ -0,0 +1,20 @@
+package java8_07;
+
+public class Day0323_05 {
+
+	public static void main(String[] args) {
+		
+		int sum = 0, add = 1;
+		
+		for(int i = 3; i <= 50; i = add*3 )
+		{
+			sum += i;
+			add++;
+		}
+		System.out.println("1에서 50까지의 수 중에 3의 배수의 합 :" + sum);
+		
+		
+
+	}
+
+}
